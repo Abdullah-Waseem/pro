d41674c41eb27526b02d3fc1f7f58399c9c10859
Abdullah Waseem Afzal <57444540+Abdullah-Waseem@users.noreply.github.com>
2025-05-09 14:24:21 +0500

    Added candle svg and logic in select to choose svgs instead of text for candle select

--- src/component/select/index.tsx
+++ src/component/select/index.tsx
@@ -12,66 +12,78 @@
  * limitations under the License.
  */
 
-import { createSignal, Component, JSX } from 'solid-js'
+import { createSignal, Component, JSX } from "solid-js";
 
 export interface SelectDataSourceItem {
-  key: string
-  text: JSX.Element
+  key: string;
+  text: JSX.Element;
 }
 
 export interface SelectProps {
-  class?: string
-  style?: JSX.CSSProperties | string
-  value?: JSX.Element
-  valueKey?: string
-  dataSource?: SelectDataSourceItem[] | string[]
-  onSelected?: (data: SelectDataSourceItem | string) => void
+  class?: string;
+  style?: JSX.CSSProperties | string;
+  value?: JSX.Element;
+  valueKey?: string;
+  dataSource?: SelectDataSourceItem[] | string[];
+  onSelected?: (data: SelectDataSourceItem | string) => void;
 }
 
-const Select: Component<SelectProps> = props => {
-  const [open, setOpen] = createSignal(false)
+const Select: Component<SelectProps> = (props) => {
+  const [open, setOpen] = createSignal(false);
 
   return (
     <div
       style={props.style}
-      class={`klinecharts-pro-select ${props.class ?? ''} ${open() ? 'klinecharts-pro-select-show' : ''}`}
+      class={`klinecharts-pro-select ${props.class ?? ""} ${
+        open() ? "klinecharts-pro-select-show" : ""
+      }`}
       tabIndex="0"
-      onClick={_ => { setOpen(o => !o) }}
-      onBlur={_ => { setOpen(false) }}>
-      <div
-        class="selector-container">
-        <span class="value">{props.value}</span>
-        <i class="arrow"/>
+      onClick={(_) => {
+        setOpen((o) => !o);
+      }}
+      onBlur={(_) => {
+        setOpen(false);
+      }}
+    >
+      <div class="selector-container">
+        {props.class == "candle-select" ? (
+          <img
+            src={`/public/candleTypesIcons/${props.value}.svg`}
+            height={20}
+          />
+        ) : (
+          <span class="value">{props.value}</span>
+        )}
+        <i class="arrow" />
       </div>
-      {
-        (props.dataSource && props.dataSource.length > 0) &&
-        <div
-          class="drop-down-container">
+      {props.dataSource && props.dataSource.length > 0 && (
+        <div class="drop-down-container">
           <ul>
-            {
-              props.dataSource.map(data => {
-                const d = data as SelectDataSourceItem
-                // @ts-expect-error
-                const v = d[props.valueKey ?? 'text'] ?? data
-                return (
-                  <li
-                    onClick={e => {
-                      e.stopPropagation()
-                      if (props.value !== v) {
-                        props.onSelected?.(data)
-                      }
-                      setOpen(false)
-                    }}>
-                    {v}
-                  </li>
-                )
-              })
-            }
+            {props.dataSource.map((data) => {
+              const d = data as SelectDataSourceItem;
+              // @ts-expect-error
+              const v = d[props.valueKey ?? "text"] ?? data;
+              // @ts-expect-error
+              const icon = d.icon;
+              return (
+                <li
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    if (props.value !== v) {
+                      props.onSelected?.(data);
+                    }
+                    setOpen(false);
+                  }}
+                >
+                  {icon ? <img src={icon} height={25} /> : <div>{v}</div>}
+                </li>
+              );
+            })}
           </ul>
         </div>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Select
+export default Select;
