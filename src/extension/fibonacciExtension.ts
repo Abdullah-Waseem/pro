738cfba0c23ce220e480dbd3577d19e79ea6ab91
Abdullah Waseem Afzal <57444540+Abdullah-Waseem@users.noreply.github.com>
2025-06-12 15:43:26 +0500

    Hard Coded Fibonacci segment and extension decimal points for price. Due to unidentifable error

--- src/extension/fibonacciExtension.ts
+++ src/extension/fibonacciExtension.ts
@@ -12,54 +12,62 @@
  * limitations under the License.
  */
 
-import { OverlayTemplate, LineAttrs, TextAttrs } from 'klinecharts'
+import { OverlayTemplate, LineAttrs, TextAttrs } from "klinecharts";
 
 const fibonacciExtension: OverlayTemplate = {
-  name: 'fibonacciExtension',
+  name: "fibonacciExtension",
   totalStep: 4,
   needDefaultPointFigure: true,
   needDefaultXAxisFigure: true,
   needDefaultYAxisFigure: true,
-  createPointFigures: ({ coordinates, overlay, precision }) => {
-    const fbLines: LineAttrs[] = []
-    const texts: TextAttrs[] = []
+  createPointFigures: ({ coordinates, overlay }) => {
+    const fbLines: LineAttrs[] = [];
+    const texts: TextAttrs[] = [];
     if (coordinates.length > 2) {
-      const points = overlay.points
+      const points = overlay.points;
       // @ts-expect-error
-      const valueDif = points[1].value - points[0].value
-      const yDif = coordinates[1].y - coordinates[0].y
-      const percents = [0, 0.236, 0.382, 0.5, 0.618, 0.786, 1]
-      const textX = coordinates[2].x > coordinates[1].x ? coordinates[1].x : coordinates[2].x
-      percents.forEach(percent => {
-        const y = coordinates[2].y + yDif * percent
+      const valueDif = points[1].value - points[0].value;
+      const yDif = coordinates[1].y - coordinates[0].y;
+      const percents = [0, 0.236, 0.382, 0.5, 0.618, 0.786, 1];
+      const textX =
+        coordinates[2].x > coordinates[1].x
+          ? coordinates[1].x
+          : coordinates[2].x;
+      percents.forEach((percent) => {
+        const y = coordinates[2].y + yDif * percent;
         // @ts-expect-error
-        const price = (points[2].value + valueDif * percent).toFixed(precision.price)
-        fbLines.push({ coordinates: [{ x: coordinates[1].x, y }, { x: coordinates[2].x, y }] })
+        const price = (points[2].value + valueDif * percent).toFixed(4);
+        fbLines.push({
+          coordinates: [
+            { x: coordinates[1].x, y },
+            { x: coordinates[2].x, y },
+          ],
+        });
         texts.push({
           x: textX,
           y,
           text: `${price} (${(percent * 100).toFixed(1)}%)`,
-          baseline: 'bottom'
-        })
-      })
+          baseline: "bottom",
+        });
+      });
     }
     return [
       {
-        type: 'line',
+        type: "line",
         attrs: { coordinates },
-        styles: { style: 'dashed' }
+        styles: { style: "dashed" },
       },
       {
-        type: 'line',
-        attrs: fbLines
+        type: "line",
+        attrs: fbLines,
       },
       {
-        type: 'text',
+        type: "text",
         ignoreEvent: true,
-        attrs: texts
-      }
-    ]
-  }
-}
+        attrs: texts,
+      },
+    ];
+  },
+};
 
-export default fibonacciExtension
+export default fibonacciExtension;
--- src/extension/fibonacciSegment.ts
+++ src/extension/fibonacciSegment.ts
@@ -12,48 +12,57 @@
  * limitations under the License.
  */
 
-import { OverlayTemplate, LineAttrs, TextAttrs } from 'klinecharts'
+import { OverlayTemplate, LineAttrs, TextAttrs } from "klinecharts";
 
 const fibonacciSegment: OverlayTemplate = {
-  name: 'fibonacciSegment',
+  name: "fibonacciSegment",
   totalStep: 3,
   needDefaultPointFigure: true,
   needDefaultXAxisFigure: true,
   needDefaultYAxisFigure: true,
-  createPointFigures: ({ coordinates, overlay, precision }) => {
-    const lines: LineAttrs[] = []
-    const texts: TextAttrs[] = []
+  createPointFigures: ({ coordinates, overlay }) => {
+    const lines: LineAttrs[] = [];
+    const texts: TextAttrs[] = [];
     if (coordinates.length > 1) {
-      const textX = coordinates[1].x > coordinates[0].x ? coordinates[0].x : coordinates[1].x
-      const percents = [1, 0.786, 0.618, 0.5, 0.382, 0.236, 0]
-      const yDif = coordinates[0].y - coordinates[1].y
-      const points = overlay.points
+      const textX =
+        coordinates[1].x > coordinates[0].x
+          ? coordinates[0].x
+          : coordinates[1].x;
+      const percents = [1, 0.786, 0.618, 0.5, 0.382, 0.236, 0];
+      const yDif = coordinates[0].y - coordinates[1].y;
+      const points = overlay.points;
       // @ts-expect-error
-      const valueDif = points[0].value - points[1].value
-      percents.forEach(percent => {
-        const y = coordinates[1].y + yDif * percent
+      const valueDif = points[0].value - points[1].value;
+      percents.forEach((percent) => {
+        const y = coordinates[1].y + yDif * percent;
         // @ts-expect-error
-        const price = (points[1].value + valueDif * percent).toFixed(precision.price)
-        lines.push({ coordinates: [{ x: coordinates[0].x, y }, { x: coordinates[1].x, y }] })
+        const price = (points[1].value + valueDif * percent).toFixed(4);
+        lines.push({
+          coordinates: [
+            { x: coordinates[0].x, y },
+            { x: coordinates[1].x, y },
+          ],
+        });
         texts.push({
           x: textX,
           y,
           text: `${price} (${(percent * 100).toFixed(1)}%)`,
-          baseline: 'bottom'
-        })
-      })
+          baseline: "bottom",
+        });
+      });
     }
     return [
       {
-        type: 'line',
-        attrs: lines
-      }, {
-        type: 'text',
+        type: "line",
+        attrs: lines,
+      },
+      {
+        type: "text",
         ignoreEvent: true,
-        attrs: texts
-      }
-    ]
-  }
-}
+        attrs: texts,
+      },
+    ];
+  },
+};
 
-export default fibonacciSegment
+export default fibonacciSegment;
